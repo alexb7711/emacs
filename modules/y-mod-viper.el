2f92db4fff7a11031a1159dcb6d3ca6720b11729
Alexander Brown <alex.brown7711@gmail.com>
2024-02-08 12:42:50 -0700

    Inhibit startup message and set level

--- modules/y-mod-viper.el
+++ modules/y-mod-viper.el
@@ -20,7 +20,12 @@
  :init
 
  ;; Enable `viper'
- (setq viper-mode t) ; Enable `viper'
+ (setq
+  viper-mode t
+  viper-inhibit-startup-message 't ; Don't ask to start `viper'
+  viper-expert-level '3 ; Set level of `viper'
+  )
+
  (require 'viper)
 
  ;; Setup defaults
@@ -31,14 +36,11 @@
  ;; Setup configuration
  (setq
   viper-electric-mode t ; Enable electric mode in viper
-  viper-inhibit-startup-message 't ; Don't ask to start `viper'
   viper-want-ctl-h-help t ; `C-h' is for help
   viper-ex-style-motion nil ; Move to start/end of lines
   viper-ex-style-editing nil ; Edit to start/end of lines
   viper-shift-width 4 ; > and < shift amount
-  viper-smart-suffix-list '("" "tex" "c" "cc" "el" "p" "py" "rs" "sh")
-  viper-expert-level '3 ; Set level of `viper'
-  ))
+  viper-smart-suffix-list '("" "tex" "c" "cc" "el" "p" "py" "rs" "sh")))
 
 ;;==============================================================================
 ;; Advice
